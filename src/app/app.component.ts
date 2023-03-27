8612304747fe7a9e7f0ccb91c41c0036d6b3fa55
Ankit <siliconvalleydeveloper>
2023-03-27 08:46:54 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Firestore,collection,addDoc,collectionData, doc, updateDoc, deleteDoc} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-fire';
+  userData! :  Observable<any>;
+
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
+
+
+  addData (f:any){
+    const collectionInstance =  collection(this.firestore,'users')
+    addDoc(collectionInstance,f.value).then(() =>{
+      console.log('Data added successfully');
+    }).catch((err) =>{
+      console.log(err);
+
+    });
+
+
+
+  }
+
+
+  getData (){
+    const collectionInstance = collection(this.firestore,'users');
+    collectionData(collectionInstance, {idField: 'id'}).subscribe(value =>{
+      console.log(value);
+
+    })
+    this.userData = collectionData(collectionInstance, {idField: 'id'});
+  }
+
+
+  updateData(id:string){
+    const docInstance = doc(this.firestore,'users', id);
+    const updateData={
+      name: 'updated name'
+    }
+
+    updateDoc(docInstance,updateData).then(() =>{
+      console.log('Data updated successfully');
+    }).catch((err) =>{
+      console.log(err); 
+    });
+
+
+  }
+
+  deleteData(id:string){
+    const docInstance = doc(this.firestore,'users', id);
+    deleteDoc(docInstance).then(() =>{
+
+      console.log('Data deleted successfully');
+    }).catch((err) =>{
+      console.log(err);
+    });
+
+  }
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'ang-fire-95275',
+    appId: '1:269803072452:web:a3363f853683ff1510d291',
+    storageBucket: 'ang-fire-95275.appspot.com',
+    apiKey: 'AIzaSyA5vpbSELG2BbgRpHmvGJy_W8QR0DH9hes',
+    authDomain: 'ang-fire-95275.firebaseapp.com',
+    messagingSenderId: '269803072452',
+    measurementId: 'G-QF96G8ZV2T',
+  },
+    production:false
+}
